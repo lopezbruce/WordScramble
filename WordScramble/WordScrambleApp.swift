2cf803d507392297971fb647c3219cdcfb7afdd6
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-15 08:02:41 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bruce Lopez on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
